16f5f6b9ca7fcae3804df1469c3220f697c7900d
Dnyaneshwar Vitthal Shekade <43291658+dnyanesh3@users.noreply.github.com>
2022-12-16 20:00:15 +0530

    Add files via upload

---
+++ reconng.sh
@@ -0,0 +1,31 @@
+Recong-ng
+marketplace install discovery
+marketplace install recon
+marketplace install import
+marketplace install exploitation
+marketplace install reporting
+workplaces list
+workplaces create security_breaches
+workplaces list
+marketplace install recon/domains-contacts/whois_pocs
+modules load recon/domains-contacts/whois_pocs
+options list
+run
+back
+marketplace install recon/profiles-profiles/namechk
+modules load recon/profiles-profiles/namechk
+options list
+back
+modules load recon/profiles-profiles/profiler
+options list
+run
+back
+modules load reporting/html
+option set CREATOR DNYANESHWAR-SHEKADE
+option set CUSTOMER rajeshmorya
+option set FILENAME /home/rajeshmourya.html
+options list
+run
+
+
+
